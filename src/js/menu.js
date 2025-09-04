b57f5578588545008660effaf4c0add5aae96fd7
Ivan Fotin <xzvtvxs@gmail.com>
2025-09-05 01:21:33 +0300

    V2.0
    -added closing the menu when clicking on a link
    -add lazy scrols
    -fixed the problem of displaying the image in the quality section
    -added email validator
    -fixed the display of the iframe
    -corrected the display of the footer section for the mobile version

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,19 +1,41 @@
-(() => {
-  const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector('[data-menu-open]'),
-    // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector('[data-menu-close]'),
-    // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector('[data-menu]'),
-  };
+// (() => {
+//   const refs = {
+//     // Додати атрибут data-modal-open на кнопку відкриття
+//     openModalBtn: document.querySelector('[data-menu-open]'),
+//     // Додати атрибут data-modal-close на кнопку закриття
+//     closeModalBtn: document.querySelector('[data-menu-close]'),
+//     // Додати атрибут data-modal на бекдроп модалки
+//     modal: document.querySelector('[data-menu]'),
+//   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle('is-open');
-  }
+//   function toggleModal() {
+//     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+//     refs.modal.classList.toggle('is-open');
+//   }
   
-})();
+// })();
+
+const menuSection = document.getElementById('mobile-menu');
+const closeBtn = document.getElementById('data-burger-close');
+
+// Закриття меню по кнопці
+closeBtn.addEventListener('click', () => {
+  menuSection.classList.remove('is-open');
+});
+
+// Відкриття меню по бургер-кнопці
+const openBtn = document.getElementById('data-burger-open');
+openBtn.addEventListener('click', () => {
+  menuSection.classList.add('is-open');
+});
+
+// Закриття меню при кліку на пункт меню
+const menuLinks = document.querySelectorAll('.nav-link');
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menuSection.classList.remove('is-open');
+  });
+});
